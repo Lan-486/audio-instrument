6ee884a02276d9d43ada1b579d5ede42a0924f3a
Lan-486 <s4084292@student.rmit.edu.au>
2026-08-12 15:00:33 +1000

    12.8.2026 class

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
+// browser loads html > brower loads js > open the dialog > user closes the dialog > 
+//  audio system loads > user clicks the sound button
+// find our dialog
+
+// find our dialog close button
+const introDialog = document.getElementById("intro-dialog");
+const introDialogCloseButton =
+  document.getElementById("intro-dialog-close");
+
+//show the found element in our beowser console
+//console.log(introDialog);
 // find out test button
-const testbutton = document.getElementById("test-button");
+const testButton = document.getElementById("test-button");
 // init our synth
 const synth= new Tone.Synth().toDestination();
 
+////// dialog
+introDialog.showModal();
+introDialogCloseButton.addEventListener("click", function () {
+  introDialog.close();
+});
+// whenever dialog closes, initialise the audio system
+introDialog.addEventListener("close", toneInit);
+
+// we put the whole function inside the event listener instead
+// as it's only called there
+// function closeIntroDialog() {
+//   introDialog.close();
+// }
+
+/////// Tone
+
+// run to setup our audio system
+function toneInit() {
+  synth.connect(Tone.Destination);
+}
 //do something when the button is clicked
-testbutton.addEventListener("click", playnote);
+testButton.addEventListener("click", playnote);
 
     //fuction to run when the button is clicked
     function playnote() {
         //play a note for a duration
-        synth.triggerAttackRelease('C4', '8n');
+        synth.triggerAttackRelease("C4", "8n");
     }
     
